17b2ce263a6ec8838cae9a5afd405971a15b5927
yang <cryptopunkyang@pm.me>
2021-08-06 21:48:44 +0200

    Add tcp app support

--- go.mod
+++ go.mod
@@ -1,13 +1,12 @@
-module github.com/cryptopunkscc/astrald/cmd
+module github.com/cryptopunkscc/astrald
 
 go 1.16
 
 require (
 	github.com/btcsuite/btcd v0.22.0-beta
-	github.com/cretz/bine v0.1.0
-	github.com/cryptopunkscc/astrald v0.0.0-20210813205828-073b751aef3f
 	github.com/go-yaml/yaml v2.1.0+incompatible
 	github.com/google/uuid v1.3.0
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
-	gopkg.in/yaml.v2 v2.4.0
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ services/appsupport/tcp/tcpappsupport.go
@@ -0,0 +1,197 @@
+package tcpappsupport
+
+import (
+	"context"
+	"fmt"
+	"github.com/cryptopunkscc/astrald/api"
+	"github.com/cryptopunkscc/astrald/node"
+	"github.com/cryptopunkscc/astrald/services/appsupport/proto"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+const tcpAppSupportAddress = "127.0.0.1:8625"
+
+func init() {
+	_ = node.RegisterService("tcp", &Runner{})
+}
+
+
+type Runner struct {}
+
+func (runner *Runner) Run(ctx context.Context, core api.Core) error {
+	network := core.Network()
+
+	go func() {
+		p, _ := network.Register("appstcp")
+
+		for r := range p.Requests() {
+			c := r.Accept()
+			var buf [4096]byte
+			c.Read(buf[:])
+		}
+	}()
+
+	// Prepare the control socket
+	ctl, err := makeSocket(tcpAppSupportAddress)
+	if err != nil {
+		return fmt.Errorf("error creating ctl socket: %v", err)
+	}
+
+	log.Println("appsupport socket:", ctl.Addr().String())
+
+	defer func() {
+		ctl.Close()
+		os.Remove(ctl.Addr().String())
+	}()
+
+	go func() {
+		<-ctx.Done()
+		ctl.Close()
+	}()
+
+	for inConn := range accept(ctl) {
+		go handleCtl(network, inConn)
+	}
+
+	return nil
+}
+
+func handleCtl(network api.Network, client *proto.Socket) error {
+	request, err := client.ReadRequest()
+	if err != nil {
+		return err
+	}
+
+	switch request.Type {
+	case proto.RequestConnect:
+		conn, err := network.Connect(api.Identity(request.Identity), request.Port)
+		if err != nil {
+			return client.Error(err.Error())
+		}
+		client.OK()
+		join(client, conn)
+	case proto.RequestRegister:
+		// Register the requested port
+		port, err := network.Register(request.Port)
+		if err != nil {
+			return client.Error(err.Error())
+		}
+		client.OK()
+		go handlePort(port, request.Path)
+		go func() {
+			for {
+				var buf [4096]byte
+				_, err := client.Read(buf[:])
+				if err != nil {
+					port.Close()
+					return
+				}
+			}
+		}()
+	default:
+		client.Close() // ignore unknown requests
+	}
+	return nil
+}
+
+func handlePort(port api.PortHandler, path string) error {
+	defer port.Close()
+
+	for request := range port.Requests() {
+		log.Println(request.Caller(), "calling", request.Query())
+
+		unix, err := net.Dial("unix", path)
+		if err != nil {
+			request.Reject()
+			return err
+		}
+
+		outConn := proto.NewSocket(unix)
+		res, err := outConn.Connect(string(request.Caller()), request.Query())
+		if err != nil {
+			request.Reject()
+			return err
+		}
+
+		// If connection was not accepted move on to the next request
+		if res.Status != proto.StatusOK {
+			request.Reject()
+			outConn.Close()
+			continue
+		}
+
+		inConn := request.Accept()
+		join(inConn, outConn)
+	}
+	return nil
+}
+
+// accept takes new connections from a listener and returns them through a channel
+func accept(l net.Listener) <-chan *proto.Socket {
+	output := make(chan *proto.Socket)
+
+	go func() {
+		defer close(output)
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				return
+			}
+
+			output <- proto.NewSocket(conn)
+		}
+	}()
+
+	return output
+}
+
+func join(a, b io.ReadWriteCloser) {
+	go func() {
+		io.Copy(a, b)
+		a.Close()
+	}()
+	go func() {
+		io.Copy(b, a)
+		b.Close()
+	}()
+}
+
+// makeSocket creates a new unix socket and returns its listener. If no name is provided a random one is used.
+func makeSocket(address string) (net.Listener, error) {
+	//if name == "" {
+	//	name = uuid.New().String()
+	//}
+	//
+	//fullPath := socketPath(name)
+
+	listen, err := net.Listen("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+
+	return listen, nil
+}
+//
+//func socketPath(name string) string {
+//	return filepath.Join(astralDir(), name)
+//}
+//
+//// TODO: This should be injected by the node
+//func astralDir() string {
+//	cfgDir, err := os.UserConfigDir()
+//	if err != nil {
+//		return defaultAstralDir
+//	}
+//
+//	dir := filepath.Join(cfgDir, "astrald")
+//	err = os.MkdirAll(dir, 0700)
+//	if err != nil {
+//		fmt.Println("astrald dir erreror:", err)
+//		return defaultAstralDir
+//	}
+//
+//	return dir
+//}
