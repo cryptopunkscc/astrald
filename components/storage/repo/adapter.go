489cba915de7616694ea5b06a249bbdfe65ed74a
yang <cryptopunkyang@pm.me>
2021-08-17 13:11:59 +0200

    Simplify repo adapter

--- components/storage/repo/adapter.go
+++ components/storage/repo/adapter.go
@@ -10,71 +10,59 @@ import (
 const tag = "file-storage"
 
 type adapter struct {
-	delegate storage.Storage
+	storage.Storage
 }
 
 type reader struct {
-	delegate storage.FileReader
+	storage.FileReader
 }
 
 type writer struct {
-	resolver fid.Resolver
-	delegate storage.FileWriter
+	fid.Resolver
+	storage.FileWriter
 }
 
 func NewAdapter(storage storage.Storage) repo.Repository {
-	return adapter{delegate: storage}
+	return adapter{Storage: storage}
 }
 
 func (f adapter) Reader(id fid.ID) (repo.Reader, error) {
-	r, err := f.delegate.Reader(id.String())
+	r, err := f.Storage.Reader(id.String())
 	if err != nil {
 		return nil, err
 	}
-	return &reader{delegate: r}, nil
-}
-
-func (r reader) Size() (int64, error) {
-	return r.delegate.Size()
+	return &reader{FileReader: r}, nil
 }
 
 func (f adapter) Writer() (repo.Writer, error) {
-	w, err := f.delegate.Writer()
+	w, err := f.Storage.Writer()
 	if err != nil {
 		return nil, err
 	}
 	return &writer{
-		resolver: fid.NewResolver(),
-		delegate: w,
+		Resolver: fid.NewResolver(),
+		FileWriter: w,
 	}, nil
 }
 
-func (r reader) Read(p []byte) (int, error) {
-	return r.delegate.Read(p)
-}
-
-func (r reader) Close() error {
-	return r.delegate.Close()
-}
-
 func (w writer) Write(p []byte) (int, error) {
-	_, _ = w.resolver.Write(p)
-	return w.delegate.Write(p)
+	_, _ = w.Resolver.Write(p)
+	return w.FileWriter.Write(p)
 }
 
 func (w writer) Finalize() (*fid.ID, error) {
-	err := w.delegate.Sync()
+	err := w.Sync()
 	if err != nil {
 		log.Println(err)
 		return nil, err
 	}
-	id := w.resolver.Resolve()
-	err = w.delegate.Rename(id.String())
+	id := w.Resolve()
+	err = w.Rename(id.String())
 	if err != nil {
 		log.Println(tag, "Cannot rename file", err)
 		return nil, err
 	}
-	err = w.delegate.Close()
+	err = w.Close()
 	if err != nil {
 		log.Println(tag, "Cannot close file", err)
 		return nil, err
