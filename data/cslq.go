cc0a59c9db029fd256c5efb297c5d68162e4ee33
arashi <arashi@cryptopunks.cc>
2022-03-04 14:46:25 +0100

    data: added cslq marshal/unmarshal

---
+++ data/cslq.go
@@ -0,0 +1,21 @@
+package data
+
+import "github.com/cryptopunkscc/astrald/cslq"
+
+var _ cslq.Marshaler = &ID{}
+var _ cslq.Unmarshaler = &ID{}
+
+func (id *ID) UnmarshalCSLQ(dec *cslq.Decoder) (err error) {
+	var buf [40]byte
+	if err = dec.Decode("[40]c", &buf); err != nil {
+		return
+	}
+
+	*id = Unpack(buf)
+
+	return
+}
+
+func (id ID) MarshalCSLQ(enc *cslq.Encoder) error {
+	return enc.Encode("[40]c", id.Pack())
+}
