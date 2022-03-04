e41db965d847204490e1321a3e51b75d1d44ba92
arashi <arashi@cryptopunks.cc>
2022-03-04 14:47:03 +0100

    cslq: added io.Closer support to Endec

--- cslq/endec.go
+++ cslq/endec.go
@@ -1,17 +1,35 @@
 package cslq
 
-import "io"
+import (
+	"errors"
+	"io"
+)
 
 // Endec represents a struct which is simultanously Encoder and a Decoder.
 type Endec struct {
 	*Encoder
 	*Decoder
+	io.Closer
 }
 
-// NewEndec returns a new Endec over the provided io.ReadWriter
+// NewEndec returns a new Endec over the provided io.ReadWriter.
 func NewEndec(rw io.ReadWriter) *Endec {
-	return &Endec{
+	e := &Endec{
 		Encoder: NewEncoder(rw),
 		Decoder: NewDecoder(rw),
 	}
+	if closer, ok := rw.(io.Closer); ok {
+		e.Closer = closer
+	}
+	return e
+}
+
+// Close closes the underlying transport. Returns an error if the transport doesn't satisfy
+// io.Closer.
+func (endec *Endec) Close() error {
+	if endec.Closer == nil {
+		return errors.New("unsupported")
+	}
+
+	return endec.Closer.Close()
 }
