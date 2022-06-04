11e78f1a0703e11e41491443bc0191d995502a31
arashi <arashi@cryptopunks.cc>
2022-06-04 23:20:15 +0200

    io:store: added values

--- proto/store/store.go
+++ proto/store/store.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"errors"
 	"github.com/cryptopunkscc/astrald/data"
 	"github.com/cryptopunkscc/astrald/proto/block"
 )
@@ -9,3 +10,13 @@ type Store interface {
 	Open(id data.ID, flags uint32) (block.Block, error)
 	Create(alloc uint64) (block.Block, string, error)
 }
+
+// ErrNotFound - block was not found in the store
+var ErrNotFound = errors.New("not found")
+
+// ErrUnsupported - operation not supported by the store
+var ErrUnsupported = errors.New("unsupported")
+
+const (
+	OpenRemote = 1 << iota
+)
