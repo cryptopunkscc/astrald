e04c5e1973a490f157a24e20793a2977dd4095cb
yang <cryptopunkyang@pm.me>
2022-09-03 17:36:23 +0200

    astral:android add wait group to service runner

--- mobile/android/node/srv.go
+++ mobile/android/node/srv.go
@@ -6,11 +6,12 @@ import (
 	"github.com/cryptopunkscc/astrald/lib/wrapper/embedded"
 	"github.com/cryptopunkscc/astrald/node"
 	"log"
+	"sync"
 	"time"
 )
 
-func serviceRunner(name string, runners ...ServiceRunner) node.ModuleRunner {
-	return embeddedServicesRunner{name, runners}
+func serviceRunner(wg *sync.WaitGroup, name string, runners ...ServiceRunner) node.ModuleRunner {
+	return embeddedServicesRunner{wg, name, runners}
 }
 
 type ServiceRunner interface {
@@ -18,6 +19,7 @@ type ServiceRunner interface {
 }
 
 type embeddedServicesRunner struct {
+	wg      *sync.WaitGroup
 	name    string
 	runners []ServiceRunner
 }
@@ -28,21 +30,26 @@ func (e embeddedServicesRunner) String() string {
 
 func (e embeddedServicesRunner) Run(ctx context.Context, n *node.Node) error {
 	api := &embedded.Adapter{Ctx: ctx, Node: n}
+	e.wg.Add(len(e.runners))
 	for _, r := range e.runners {
 		runner := r
 		go func() {
+			defer e.wg.Done()
 			counter := 0
 			var err error
 			for counter < 10 {
 				start := time.Now().UnixMilli()
 				err = runner.Run(ctx, api)
+				if err != nil || ctx.Err() != nil {
+					log.Println("service", runner, "finished error:", err)
+					return
+				}
 				end := time.Now().UnixMilli()
 				if end-start < 2000 {
 					counter++
 				} else {
 					counter = 0
 				}
-				log.Println(runner, "run error:", err)
 			}
 			log.Println(runner, "aborted because failing constantly")
 		}()
