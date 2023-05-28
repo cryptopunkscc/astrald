c433b8342fe3fac5083ee6177bebf7421a0e57fe
arashi <arashi@cryptopunks.cc>
2023-05-28 20:05:20 +0200

    mod/storage: added more errors to the protocol

--- mod/storage/proto/proto.go
+++ mod/storage/proto/proto.go
@@ -7,7 +7,22 @@ import (
 
 var es stream.ErrorSpace
 
-var ErrUnavailable = es.NewError(0x01, "unavailable")
+var (
+	// ErrUnavailable - requested data is not available
+	ErrUnavailable = es.NewError(0x01, "unavailable")
+
+	// ErrSeekUnsupported - seek operation is not supported
+	ErrSeekUnsupported = es.NewError(0x02, "seek unsupported")
+
+	// ErrSizeMismatch - size from data.ID and actual data size differ
+	ErrSizeMismatch = es.NewError(0x03, "block size mismatch")
+
+	// ErrInvalidOffset - offset is less than zero or more than the data length
+	ErrInvalidOffset = es.NewError(0x04, "invalid offset")
+
+	// ErrInvalidLength - length exceeds the remaining data or lenght is less than 1
+	ErrInvalidLength = es.NewError(0x05, "invalid length")
+)
 
 func New(rw io.ReadWriter) *stream.Stream {
 	var s = stream.NewStream(rw, es)
