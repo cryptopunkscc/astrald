d217421783b515e572b4a28df3da4b8cbf9e1395
arashi <arashi@cryptopunks.cc>
2023-09-28 21:08:29 +0200

    streams: added NilCloser and NilWriteCloser

--- streams/nil_writer.go
+++ streams/nil_writer.go
@@ -6,3 +6,15 @@ type NilWriter struct {
 func (NilWriter) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
+
+type NilCloser struct {
+}
+
+func (NilCloser) Close() error {
+	return nil
+}
+
+type NilWriteCloser struct {
+	NilWriter
+	NilCloser
+}
