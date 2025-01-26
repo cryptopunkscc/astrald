ac557e47bf4c3ecd69b54ad6b3840a2e5aef26e8
arashi <arashi@cryptopunks.cc>
2025-01-26 16:34:41 +0100

    sig: add comments

--- sig/context.go
+++ sig/context.go
@@ -4,6 +4,8 @@ import (
 	"context"
 )
 
+// Context executes the function fn. If the function finishes before context ends, Context returns nil.
+// If the context ends before the function finishes, Context does not wait for the function and returns ctx.Err().
 func Context(ctx context.Context, fn func()) error {
 	ch := make(chan error, 1)
 
