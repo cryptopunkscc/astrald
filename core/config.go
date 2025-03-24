b7513c5b567e0b44f4d8c8efcace0d7715e3b3f4
yang <cryptopunkyang@pm.me>
2025-03-24 20:45:18 +0100

    config: add omitempty to core.Config and apphost.Config

--- core/config.go
+++ core/config.go
@@ -3,15 +3,15 @@ package core
 const configName = "node"
 
 type Config struct {
-	Identity        string   `yaml:"identity"`
-	Modules         []string `yaml:"modules"`
-	LogRoutingStart bool     `yaml:"log_routing_start"`
+	Identity        string   `yaml:"identity,omitempty"`
+	Modules         []string `yaml:"modules,omitempty"`
+	LogRoutingStart bool     `yaml:"log_routing_start,omitempty"`
 	Log             LogConfig
 }
 
 type LogConfig struct {
-	Level         int  `yaml:"level"`
-	DisableColors bool `yaml:"disable_colors"`
+	Level         int  `yaml:"level,omitempty"`
+	DisableColors bool `yaml:"disable_colors,omitempty"`
 }
 
 var defaultConfig = Config{}
