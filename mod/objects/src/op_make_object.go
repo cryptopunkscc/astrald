2036f49f9623440e46cc22a3faaabfcc5ee5237a
arashi <arashi@cryptopunks.cc>
2025-03-25 02:36:16 +0100

    mod/objects: added OpMakeObject

---
+++ mod/objects/src/op_make_object.go
@@ -0,0 +1,36 @@
+package objects
+
+import (
+	"encoding/json"
+	"github.com/cryptopunkscc/astrald/astral"
+	"github.com/cryptopunkscc/astrald/mod/shell"
+)
+
+type opMakeObjectArgs struct {
+	Type string
+}
+
+func (mod *Module) OpMakeObject(ctx astral.Context, q shell.Query, args opMakeObjectArgs) (err error) {
+	object := mod.Blueprints().Make(args.Type)
+	if object == nil {
+		mod.log.Errorv(2, "objects.make_object: unknown type %v", args.Type)
+		return q.Reject()
+	}
+
+	conn, err := q.Accept()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	err = json.NewDecoder(conn).Decode(&object)
+
+	if err != nil {
+		astral.Write(conn, astral.NewError(err.Error()), true)
+		return
+	}
+
+	astral.Write(conn, object, true)
+
+	return
+}
