9f7c3591c79b915bb64c737ca61cd969992133ef
arashi <arashi@cryptopunks.cc>
2025-04-25 15:58:24 +0200

    mod/objects: op_make_object -> op_convert

---
+++ mod/objects/src/op_convert.go
@@ -0,0 +1,34 @@
+package objects
+
+import (
+	"errors"
+	"github.com/cryptopunkscc/astrald/astral"
+	"github.com/cryptopunkscc/astrald/mod/shell"
+	"io"
+)
+
+type opConvertArgs struct {
+	From   string `query:"optional"`
+	Format string `query:"optional"`
+}
+
+func (mod *Module) OpConvert(ctx *astral.Context, q shell.Query, args opConvertArgs) (err error) {
+	chOut := astral.NewChannel(q.Accept(), args.Format)
+	chIn := astral.NewChannel(chOut.Transport(), args.From)
+	defer chOut.Close()
+
+	for {
+		object, err := chIn.Read()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return err
+			}
+			object = astral.NewError(err.Error())
+		}
+
+		err = chOut.Write(object)
+		if err != nil {
+			return err
+		}
+	}
+}
