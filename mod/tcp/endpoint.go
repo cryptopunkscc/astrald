343bd320efdc73d85f7c46bae5f116775876ee3a
arashi <arashi@cryptopunks.cc>
2025-04-25 16:07:32 +0200

    mod/tcp: improved Endpoint implementation

--- mod/tcp/endpoint.go
+++ mod/tcp/endpoint.go
@@ -15,6 +15,8 @@ import (
 var _ exonet.Endpoint = &Endpoint{}
 var _ astral.Object = &Endpoint{}
 
+// Endpoint is an astral.Object that holds information about a TCP endpoint, i.e. an IP address and a port.
+// Supports JSON and text.
 type Endpoint struct {
 	IP   IP
 	Port astral.Uint16
@@ -22,14 +24,6 @@ type Endpoint struct {
 
 func (e *Endpoint) ObjectType() string {
 	return "mod.tcp.endpoint"
-}
-
-func (e *Endpoint) Address() string {
-	return net.JoinHostPort(e.IP.String(), strconv.Itoa(int(e.Port)))
-}
-
-func (e *Endpoint) Network() string {
-	return "tcp"
 }
 
 func (e Endpoint) WriteTo(w io.Writer) (n int64, err error) {
@@ -40,6 +34,16 @@ func (e *Endpoint) ReadFrom(r io.Reader) (n int64, err error) {
 	return streams.ReadAllFrom(r, &e.IP, &e.Port)
 }
 
+// exonet.Endpoint
+
+func (e *Endpoint) Address() string {
+	return net.JoinHostPort(e.IP.String(), strconv.Itoa(int(e.Port)))
+}
+
+func (e *Endpoint) Network() string {
+	return "tcp"
+}
+
 func (e *Endpoint) Pack() []byte {
 	var b = &bytes.Buffer{}
 	if _, err := e.WriteTo(b); err != nil {
@@ -47,6 +51,41 @@ func (e *Endpoint) Pack() []byte {
 	}
 	return b.Bytes()
 }
+
+// Text marshaling
+
+func (e Endpoint) MarshalText() (text []byte, err error) {
+	return []byte(e.Address()), nil
+}
+
+func (e *Endpoint) UnmarshalText(text []byte) error {
+	h, p, err := net.SplitHostPort(string(text))
+	if err != nil {
+		return err
+	}
+
+	ip, err := ParseIP(h)
+	if err != nil {
+		return err
+	}
+
+	port, err := strconv.Atoi(p)
+	if err != nil {
+		return err
+	}
+
+	// check if port fits in 16 bits
+	if (port >> 16) > 0 {
+		return errors.New("port out of range")
+	}
+
+	e.IP = ip
+	e.Port = astral.Uint16(port)
+
+	return nil
+}
+
+// ...
 
 func (e *Endpoint) String() string {
 	return e.Address()
@@ -84,6 +123,8 @@ func ParseEndpoint(s string) (*Endpoint, error) {
 }
 
 func init() {
+	astral.DefaultBlueprints.Add(&Endpoint{})
+
 	term.SetTranslateFunc(func(o *Endpoint) astral.Object {
 		return &term.ColorString{
 			Color: term.HighlightColor,
