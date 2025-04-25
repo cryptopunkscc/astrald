54c20e4dc88d703c45e4ab652d526eb6c376abe1
arashi <arashi@cryptopunks.cc>
2025-04-26 01:09:25 +0200

    mod/gateway: added json and text support to Endpoint

--- mod/gateway/src/endpoint.go
+++ mod/gateway/src/endpoint.go
@@ -2,31 +2,29 @@ package gateway
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
 	"github.com/cryptopunkscc/astrald/astral"
 	"github.com/cryptopunkscc/astrald/mod/exonet"
 	"io"
+	"strings"
 )
-
-var _ exonet.Endpoint = &Endpoint{}
-var _ astral.Object = &Endpoint{}
 
 type Endpoint struct {
 	GatewayID *astral.Identity
 	TargetID  *astral.Identity
 }
 
-var _ astral.Object = &Endpoint{}
+var _ exonet.Endpoint = &Endpoint{}
 
-func (Endpoint) ObjectType() string {
-	return "mod.gateway.endpoint"
+func (Endpoint) ObjectType() string { return "mod.gateway.endpoint" }
+
+func (e Endpoint) WriteTo(w io.Writer) (n int64, err error) {
+	return astral.Struct(e).WriteTo(w)
 }
 
-func (endpoint Endpoint) WriteTo(w io.Writer) (n int64, err error) {
-	return astral.Struct(endpoint).WriteTo(w)
-}
-
-func (endpoint *Endpoint) ReadFrom(r io.Reader) (n int64, err error) {
-	return astral.Struct(endpoint).ReadFrom(r)
+func (e *Endpoint) ReadFrom(r io.Reader) (n int64, err error) {
+	return astral.Struct(e).ReadFrom(r)
 }
 
 // NewEndpoint makes a new Endpoint
@@ -34,11 +32,22 @@ func NewEndpoint(gateway *astral.Identity, target *astral.Identity) *Endpoint {
 	return &Endpoint{GatewayID: gateway, TargetID: target}
 }
 
+// exonet.Endpoint
+
+func (e Endpoint) Network() string {
+	return NetworkName
+}
+
+// Address returns a text representation of the address
+func (e Endpoint) Address() string {
+	return e.GatewayID.String() + ":" + e.TargetID.String()
+}
+
 // Pack returns a binary representation of the address
-func (endpoint Endpoint) Pack() []byte {
+func (e Endpoint) Pack() []byte {
 	buf := &bytes.Buffer{}
 
-	_, err := endpoint.WriteTo(buf)
+	_, err := e.WriteTo(buf)
 	if err != nil {
 		panic(err)
 	}
@@ -46,19 +55,54 @@ func (endpoint Endpoint) Pack() []byte {
 	return buf.Bytes()
 }
 
-// Address returns a text representation of the address
-func (endpoint Endpoint) Address() string {
-	return endpoint.GatewayID.String() + ":" + endpoint.TargetID.String()
+// text support
+
+func (e *Endpoint) UnmarshalText(text []byte) (err error) {
+	ids := strings.SplitN(string(text), ":", 2)
+	if len(ids) != 2 {
+		return errors.New("malformed endpoint")
+	}
+
+	e.GatewayID, err = astral.IdentityFromString(ids[0])
+	if err != nil {
+		return
+	}
+
+	e.TargetID, err = astral.IdentityFromString(ids[1])
+
+	return
 }
 
-func (endpoint Endpoint) IsZero() bool {
-	return endpoint.GatewayID.IsZero() && endpoint.TargetID.IsZero()
+func (e Endpoint) MarshalText() (text []byte, err error) {
+	return []byte(e.Address()), nil
 }
 
-func (endpoint Endpoint) Network() string {
-	return NetworkName
+// json support
+
+func (e *Endpoint) UnmarshalJSON(b []byte) error {
+	var str string
+	err := json.Unmarshal(b, &str)
+	if err != nil {
+		return err
+	}
+
+	return e.UnmarshalText([]byte(str))
 }
 
-func (endpoint Endpoint) String() string {
-	return endpoint.Network() + ":" + endpoint.Address()
+func (e Endpoint) MarshalJSON() ([]byte, error) {
+	return json.Marshal(e.Address())
+}
+
+// other
+
+func (e Endpoint) IsZero() bool {
+	return e.GatewayID.IsZero() && e.TargetID.IsZero()
+}
+
+func (e Endpoint) String() string {
+	return e.Network() + ":" + e.Address()
+}
+
+func init() {
+	astral.DefaultBlueprints.Add(&Endpoint{})
 }
