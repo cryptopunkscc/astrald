2feea73b1e2bb6007fac746e36c7db5d7c100d14
arashi <arashi@cryptopunks.cc>
2025-04-28 20:09:20 +0200

    astral: added json and text to ack

--- astral/ack.go
+++ astral/ack.go
@@ -6,6 +6,8 @@ type Ack struct{}
 
 var _ Object = &Ack{}
 
+// astral
+
 func (Ack) ObjectType() string { return "ack" }
 
 func (Ack) WriteTo(w io.Writer) (n int64, err error) {
@@ -14,4 +16,28 @@ func (Ack) WriteTo(w io.Writer) (n int64, err error) {
 
 func (*Ack) ReadFrom(r io.Reader) (n int64, err error) {
 	return 0, err
+}
+
+// json
+
+func (a Ack) UnmarshalJSON(bytes []byte) error {
+	return nil
+}
+
+func (a Ack) MarshalJSON() ([]byte, error) {
+	return []byte("\"\""), nil
+}
+
+// text
+
+func (a Ack) UnmarshalText(text []byte) error {
+	return nil
+}
+
+func (a Ack) MarshalText() (text []byte, err error) {
+	return []byte{}, nil
+}
+
+func init() {
+	DefaultBlueprints.Add(&Ack{})
 }
