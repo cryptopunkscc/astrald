2ee617bc676a8f9d03256fcd966b1bac42944f73
arashi <arashi@cryptopunks.cc>
2025-05-16 14:46:12 +0200

    mod/objects: op_push: added channel support

--- mod/objects/src/op_push.go
+++ mod/objects/src/op_push.go
@@ -3,23 +3,66 @@ package objects
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"github.com/cryptopunkscc/astrald/astral"
 	"github.com/cryptopunkscc/astrald/mod/shell"
 	"github.com/cryptopunkscc/astrald/object"
 	"io"
 )
 
-const maxPushSize = 4096
+const maxPushSize = 32 * 1024
 
 type opPushArgs struct {
-	Size int
+	Size int    `query:"optional"`
+	In   string `query:"optional"`
+	Out  string `query:"optional"`
 }
 
 func (mod *Module) OpPush(ctx *astral.Context, q shell.Query, args opPushArgs) (err error) {
-	if args.Size > maxPushSize {
+	switch {
+	case args.Size > maxPushSize:
 		return q.Reject()
+	case args.Size == 0:
+		return mod.opPushStream(ctx, q, args)
+	default:
+		return mod.opPushSingle(ctx, q, args)
 	}
+}
 
+func (mod *Module) opPushStream(ctx *astral.Context, q shell.Query, args opPushArgs) (err error) {
+	ch := astral.NewChannelFmt(q.Accept(), args.In, args.Out)
+	defer ch.Close()
+
+	var acccepted, rejected int
+
+	defer func() {
+		mod.log.Logv(1, "push from %v: accepted %v, rejected %v", q.Caller(), acccepted, rejected)
+	}()
+
+	for {
+		object, err := ch.Read()
+		if err != nil {
+			if !errors.Is(err, io.EOF) {
+				mod.log.Errorv(1, "op_push: channel read: %v", err)
+			}
+			return nil
+		}
+
+		if mod.receive(q.Caller(), object) {
+			acccepted++
+			err = ch.Write(&astral.Ack{})
+		} else {
+			rejected++
+			err = ch.Write(astral.NewError("rejected"))
+		}
+		if err != nil {
+			mod.log.Errorv(1, "op_push: channel write: %v", err)
+			return nil
+		}
+	}
+}
+
+func (mod *Module) opPushSingle(ctx *astral.Context, q shell.Query, args opPushArgs) (err error) {
 	stream := q.Accept()
 	defer stream.Close()
 
