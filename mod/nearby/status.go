abe505b35bab000eaf3572056396ce2bc6a587a7
arashi <arashi@cryptopunks.cc>
2025-06-09 15:37:00 +0200

    mod/nearby: bugfix

--- mod/nearby/status.go
+++ mod/nearby/status.go
@@ -17,7 +17,7 @@ func (s Status) WriteTo(w io.Writer) (n int64, err error) {
 	return astral.Struct(s).WriteTo(w)
 }
 
-func (s Status) ReadFrom(r io.Reader) (n int64, err error) {
+func (s *Status) ReadFrom(r io.Reader) (n int64, err error) {
 	return astral.Struct(s).ReadFrom(r)
 }
 
