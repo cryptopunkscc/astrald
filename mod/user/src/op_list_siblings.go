21a414309ab5f47b8194a315d9b326bbdb1eed71
yang <cryptopunkyang@pm.me>
2025-07-17 16:51:03 +0200

    mod/user: add op_list_siblings.go

---
+++ mod/user/src/op_list_siblings.go
@@ -0,0 +1,27 @@
+package user
+
+import (
+	"github.com/cryptopunkscc/astrald/astral"
+	"github.com/cryptopunkscc/astrald/mod/shell"
+)
+
+type opScanArgs struct {
+	Out  string      `query:"optional"`
+	Zone astral.Zone `query:"optional"`
+}
+
+func (mod *Module) OpListSiblings(ctx *astral.Context, q shell.Query, args opScanArgs) (err error) {
+	ctx, cancel := ctx.WithIdentity(q.Caller()).IncludeZone(args.Zone).WithCancel()
+	defer cancel()
+
+	ch := astral.NewChannelFmt(q.Accept(), "", args.Out)
+	defer ch.Close()
+
+	for _, id := range mod.listSibs() {
+		err = ch.Write(id)
+		if err != nil {
+			return
+		}
+	}
+	return
+}
