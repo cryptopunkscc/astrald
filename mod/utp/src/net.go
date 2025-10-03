8b13270fa65e9ce2187a5651a2aeb6daea5dd6a8
Rekseto <grzegorz.dyl.private@gmail.com>
2025-10-03 15:34:35 +0200

    add utp module

--- mod/utp/src/net.go
+++ mod/utp/src/net.go
@@ -1,7 +1,1 @@
 package utp
-
-import "net"
-
-// NOTE: same as in tcp module - consider moving to a common package
-
-var InterfaceAddrs = net.InterfaceAddrs
--- mod/utp/src/parse.go
+++ mod/utp/src/parse.go
@@ -1,16 +1,1 @@
 package utp
-
-import (
-	"github.com/cryptopunkscc/astrald/mod/exonet"
-	"github.com/cryptopunkscc/astrald/mod/utp"
-)
-
-func (mod *Module) Parse(network string, address string) (exonet.Endpoint, error) {
-	switch network {
-	case "utp":
-	default:
-		return nil, exonet.ErrUnsupportedNetwork
-	}
-
-	return utp.ParseEndpoint(address)
-}
