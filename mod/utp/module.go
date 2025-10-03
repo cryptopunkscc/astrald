462fff0bab7ed085c95755656cde6f0112233383
Rekseto <grzegorz.dyl.private@gmail.com>
2025-10-03 15:34:35 +0200

    add utp module

--- mod/utp/module.go
+++ mod/utp/module.go
@@ -1,14 +1,1 @@
 package utp
-
-import (
-	"github.com/cryptopunkscc/astrald/mod/exonet"
-)
-
-const ModuleName = "utp"
-
-type Module interface {
-	exonet.Dialer
-	exonet.Unpacker
-	exonet.Parser
-	ListenPort() int
-}
--- mod/utp/src/config.go
+++ mod/utp/src/config.go
@@ -1,16 +1,1 @@
 package utp
-
-import (
-	"time"
-)
-
-type Config struct {
-	ListenPort      int           `yaml:"listen_port,omitempty"` // Port to listen on for incoming connections (default 1791)
-	PublicEndpoints []string      `yaml:"public_endpoints,omitempty"`
-	DialTimeout     time.Duration `yaml:"dial_timeout,omitempty"` // Timeout for dialing connections (default 1 minute)
-}
-
-var defaultConfig = Config{
-	DialTimeout: time.Minute,
-	ListenPort:  1791,
-}
