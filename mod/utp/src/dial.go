a2b3f906017e4506643fbc84241ab52aeda5bc07
Rekseto <grzegorz.dyl.private@gmail.com>
2025-10-04 16:56:34 +0200

    mod/utp: minor fixes from CR

--- mod/utp/src/dial.go
+++ mod/utp/src/dial.go
@@ -9,9 +9,8 @@ import (
 
 var _ exonet.Dialer = &Module{}
 
-// Dial establishes a reliable (utp) connection and returns it only after the
+// Dial establishes a reliable (utp) connection and wraps it as an exonet.Conn.
 func (mod *Module) Dial(ctx *astral.Context, endpoint exonet.Endpoint) (exonet.Conn, error) {
-
 	switch endpoint.Network() {
 	case "utp":
 	default:
@@ -19,17 +18,22 @@ func (mod *Module) Dial(ctx *astral.Context, endpoint exonet.Endpoint) (exonet.C
 	}
 
 	dialer := utp.Dialer{Timeout: mod.config.DialTimeout}
-	if mod.config.DialTimeout > 0 {
-		dialer.Timeout = mod.config.DialTimeout
-	}
-
 	conn, err := dialer.Dial("utp", endpoint.Address())
 	if err != nil {
 		return nil, err
 	}
 
-	localEndpoint, _ := utpmod.ParseEndpoint(conn.LocalAddr().String())
-	remoteEndpoint, _ := utpmod.ParseEndpoint(conn.RemoteAddr().String())
+	localEndpoint, err := utpmod.ParseEndpoint(conn.LocalAddr().String())
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	remoteEndpoint, err := utpmod.ParseEndpoint(conn.RemoteAddr().String())
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
 
 	return WrapUtpConn(conn, remoteEndpoint, localEndpoint, true), nil
 }
