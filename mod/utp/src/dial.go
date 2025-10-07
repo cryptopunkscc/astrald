acfc91d202c46ca3f929a38506cbd3d510d16576
Rekseto <grzegorz.dyl.private@gmail.com>
2025-10-07 15:53:46 +0200

    mod/utp: improve error handling in Dial function

--- mod/utp/src/dial.go
+++ mod/utp/src/dial.go
@@ -12,7 +12,8 @@ import (
 var _ exonet.Dialer = &Module{}
 
 // Dial establishes a reliable (utp) connection and wraps it as an exonet.Conn.
-func (mod *Module) Dial(ctx *astral.Context, endpoint exonet.Endpoint) (exonet.Conn, error) {
+func (mod *Module) Dial(ctx *astral.Context, endpoint exonet.Endpoint) (
+	c exonet.Conn, err error) {
 	switch endpoint.Network() {
 	case "utp":
 	default:
@@ -26,16 +27,21 @@ func (mod *Module) Dial(ctx *astral.Context, endpoint exonet.Endpoint) (exonet.C
 			err)
 	}
 
+	// Close raw conn on any subsequent error; noop on success.
+	defer func() {
+		if err != nil {
+			conn.Close()
+		}
+	}()
+
 	localEndpoint, err := utpmod.ParseEndpoint(conn.LocalAddr().String())
 	if err != nil {
-		conn.Close()
-		return nil, fmt.Errorf(`utpmodule/dial parsing local endpoint failed
+		return nil, fmt.Errorf(`utp module/dial parsing local endpoint failed
 : %w`, err)
 	}
 
 	remoteEndpoint, err := utpmod.ParseEndpoint(conn.RemoteAddr().String())
 	if err != nil {
-		conn.Close()
 		return nil, fmt.Errorf(`utp module/dial parsing remote endpoint failed
 : %w`, err)
 	}
