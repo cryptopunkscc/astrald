06a054f2a7b6f09027edb360c23ba7e05446dcf5
Rekseto <grzegorz.dyl.private@gmail.com>
2025-10-08 17:00:31 +0200

    mod/ip: refactor config structure and remove public endpoints in tcp & utp

--- mod/tcp/src/config.go
+++ mod/tcp/src/config.go
@@ -3,9 +3,8 @@ package tcp
 import "time"
 
 type Config struct {
-	DialTimeout     time.Duration `yaml:"dial_timeout,omitempty"`
-	PublicEndpoints []string      `yaml:"public_endpoints,omitempty"`
-	ListenPort      int           `yaml:"listen_port,omitempty"`
+	DialTimeout time.Duration `yaml:"dial_timeout,omitempty"`
+	ListenPort  int           `yaml:"listen_port,omitempty"`
 }
 
 var defaultConfig = Config{
--- mod/utp/src/config.go
+++ mod/utp/src/config.go
@@ -5,9 +5,8 @@ import (
 )
 
 type Config struct {
-	ListenPort      int           `yaml:"listen_port,omitempty"` // Port to listen on for incoming connections (default 1791)
-	PublicEndpoints []string      `yaml:"public_endpoints,omitempty"`
-	DialTimeout     time.Duration `yaml:"dial_timeout,omitempty"` // Timeout for dialing connections (default 1 minute)
+	ListenPort  int           `yaml:"listen_port,omitempty"`  // Port to listen on for incoming connections (default 1791)
+	DialTimeout time.Duration `yaml:"dial_timeout,omitempty"` // Timeout for dialing connections (default 1 minute)
 }
 
 var defaultConfig = Config{
