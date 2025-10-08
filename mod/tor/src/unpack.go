5206d7945942814a017d26cfc21c6e052466f36e
arashi <arashi@cryptopunks.cc>
2025-10-08 03:39:02 +0200

    tor: fixed endpoint unpacking

--- mod/tor/src/unpack.go
+++ mod/tor/src/unpack.go
@@ -2,11 +2,8 @@ package tor
 
 import (
 	"bytes"
-	"errors"
-	"github.com/cryptopunkscc/astrald/astral"
 	"github.com/cryptopunkscc/astrald/mod/exonet"
 	"github.com/cryptopunkscc/astrald/mod/tor"
-	"io"
 )
 
 const addrVersion = 3
@@ -21,37 +18,13 @@ func (mod *Module) Unpack(network string, data []byte) (exonet.Endpoint, error) 
 }
 
 // Unpack converts a binary representation of the address to a struct
-func Unpack(data []byte) (*tor.Endpoint, error) {
-	r := bytes.NewReader(data)
+func Unpack(data []byte) (_ *tor.Endpoint, err error) {
+	var e tor.Endpoint
 
-	var (
-		err      error
-		version  uint8
-		keyBytes = make([]byte, 35)
-		port     uint16
-	)
-
-	_, err = (*astral.Uint8)(&version).ReadFrom(r)
+	_, err = e.ReadFrom(bytes.NewReader(data))
 	if err != nil {
 		return nil, err
 	}
 
-	if version != addrVersion {
-		return nil, errors.New("invalid version")
-	}
-
-	_, err = io.ReadFull(r, keyBytes)
-	if err != nil {
-		return nil, err
-	}
-
-	_, err = (*astral.Uint16)(&port).ReadFrom(r)
-	if err != nil {
-		return nil, err
-	}
-
-	return &tor.Endpoint{
-		Digest: keyBytes,
-		Port:   astral.Uint16(port),
-	}, nil
+	return &e, nil
 }
