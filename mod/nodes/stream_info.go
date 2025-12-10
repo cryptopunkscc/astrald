12dca944021d5cc645136a521ce950e4d2fdeea7
Rekseto <grzegorz.dyl.private@gmail.com>
2025-12-10 17:17:35 +0000

    usage of astral.Objectify

--- mod/nodes/stream_info.go
+++ mod/nodes/stream_info.go
@@ -30,7 +30,12 @@ func (s StreamInfo) ObjectType() string {
 }
 
 func (s StreamInfo) WriteTo(w io.Writer) (n int64, err error) {
-	return astral.Struct(s).WriteTo(w)
+	o, err := astral.Objectify(s)
+	if err != nil {
+		return 0, err
+	}
+
+	return o.WriteTo(w)
 }
 
 func (s *StreamInfo) ReadFrom(r io.Reader) (n int64, err error) {
