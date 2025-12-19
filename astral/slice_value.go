c3f79d1f72b35400dec66f33419ebded970d4b7d
arashi <arashi@cryptopunks.cc>
2025-12-19 22:38:48 +0100

    astral: fix sliceValue

--- astral/slice_value.go
+++ astral/slice_value.go
@@ -2,22 +2,14 @@ package astral
 
 import (
 	"encoding/binary"
+	"encoding/json"
+	"errors"
 	"io"
 	"reflect"
 )
 
 type sliceValue struct {
 	reflect.Value
-}
-
-func (a sliceValue) MarshalJSON() ([]byte, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (a sliceValue) UnmarshalJSON(bytes []byte) error {
-	//TODO implement me
-	panic("implement me")
 }
 
 var _ Object = &sliceValue{}
@@ -74,4 +66,61 @@ func (a sliceValue) ReadFrom(r io.Reader) (n int64, err error) {
 		}
 	}
 	return
+}
+
+func (a sliceValue) MarshalJSON() (_ []byte, err error) {
+	var arr []json.RawMessage
+	var o Object
+	var raw []byte
+
+	for i := range a.Len() {
+		o, err = objectify(a.Index(i))
+		if err != nil {
+			return
+		}
+
+		m, ok := o.(json.Marshaler)
+		if !ok {
+			return nil, errors.New("object does not implement json encoding")
+		}
+
+		raw, err = m.MarshalJSON()
+		if err != nil {
+			return
+		}
+
+		arr = append(arr, raw)
+	}
+
+	return json.Marshal(arr)
+}
+
+func (a sliceValue) UnmarshalJSON(bytes []byte) error {
+	var arr []json.RawMessage
+
+	err := json.Unmarshal(bytes, &arr)
+	if err != nil {
+		return err
+	}
+
+	a.Set(reflect.MakeSlice(a.Type(), len(arr), len(arr)))
+
+	for i, raw := range arr {
+		o, err := objectify(a.Index(i))
+		if err != nil {
+			return err
+		}
+
+		m, ok := o.(json.Unmarshaler)
+		if !ok {
+			return errors.New("object does not implement json encoding")
+		}
+
+		err = m.UnmarshalJSON(raw)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
---
+++ astral/slice_value_test.go
@@ -0,0 +1,51 @@
+package astral
+
+import (
+	"bytes"
+	"slices"
+	"testing"
+)
+
+func TestSliceOfInts(t *testing.T) {
+	var src, dst []int
+
+	for i := 0; i < 10; i++ {
+		src = append(src, i)
+	}
+
+	srcObject := Objectify(src)
+
+	// test binary marshaling
+	var buf = &bytes.Buffer{}
+
+	_, err := srcObject.WriteTo(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dstObject := Objectify(&dst)
+	_, err = dstObject.ReadFrom(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if slices.Compare(dst, src) != 0 {
+		t.Fatal("slice values not equal")
+	}
+	dst = nil
+
+	// test json marshaling
+	json, err := srcObject.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = dstObject.UnmarshalJSON(json)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if slices.Compare(dst, src) != 0 {
+		t.Fatal("slice values not equal")
+	}
+}
