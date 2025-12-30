849ddb9d17aeae775a68a309176d7fa041c66199
arashi <arashi@cryptopunks.cc>
2025-12-30 18:57:11 +0100

    added mod/allpub that includes all public interfaces of all modules

---
+++ mod/allpub/mods.go
@@ -0,0 +1,32 @@
+package all
+
+// This file includes all public interfaces of all modules.
+
+import (
+	_ "github.com/cryptopunkscc/astrald/mod/apphost"
+	_ "github.com/cryptopunkscc/astrald/mod/archives"
+	_ "github.com/cryptopunkscc/astrald/mod/auth"
+	_ "github.com/cryptopunkscc/astrald/mod/dir"
+	_ "github.com/cryptopunkscc/astrald/mod/ether"
+	_ "github.com/cryptopunkscc/astrald/mod/events"
+	_ "github.com/cryptopunkscc/astrald/mod/exonet"
+	_ "github.com/cryptopunkscc/astrald/mod/fs"
+	//_ "github.com/cryptopunkscc/astrald/mod/fwd"
+	_ "github.com/cryptopunkscc/astrald/mod/gateway"
+	_ "github.com/cryptopunkscc/astrald/mod/ip"
+	_ "github.com/cryptopunkscc/astrald/mod/kcp"
+	_ "github.com/cryptopunkscc/astrald/mod/keys"
+	_ "github.com/cryptopunkscc/astrald/mod/kos"
+	_ "github.com/cryptopunkscc/astrald/mod/log"
+	_ "github.com/cryptopunkscc/astrald/mod/media"
+	_ "github.com/cryptopunkscc/astrald/mod/nat"
+	_ "github.com/cryptopunkscc/astrald/mod/nearby"
+	_ "github.com/cryptopunkscc/astrald/mod/nodes"
+	_ "github.com/cryptopunkscc/astrald/mod/objects"
+	_ "github.com/cryptopunkscc/astrald/mod/scheduler"
+	_ "github.com/cryptopunkscc/astrald/mod/shell"
+	_ "github.com/cryptopunkscc/astrald/mod/tcp"
+	_ "github.com/cryptopunkscc/astrald/mod/tor"
+	_ "github.com/cryptopunkscc/astrald/mod/user"
+	_ "github.com/cryptopunkscc/astrald/mod/utp"
+)
