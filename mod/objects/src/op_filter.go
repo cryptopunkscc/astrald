6ef89f0c65d909f06f7318c947a9216488dec9cd
arashi <arashi@cryptopunks.cc>
2026-01-04 21:01:14 +0100

    mod/objects: op_convert -> op_filter

---
+++ mod/objects/src/op_filter.go
@@ -0,0 +1,60 @@
+package objects
+
+import (
+	"errors"
+	"io"
+	"slices"
+	"strings"
+
+	"github.com/cryptopunkscc/astrald/astral"
+	"github.com/cryptopunkscc/astrald/astral/channel"
+	"github.com/cryptopunkscc/astrald/mod/shell"
+)
+
+type opFilterArgs struct {
+	Only   *string `query:"optional"`
+	Except *string `query:"optional"`
+	In     string  `query:"optional"`
+	Out    string  `query:"optional"`
+}
+
+func (mod *Module) OpFilter(ctx *astral.Context, q shell.Query, args opFilterArgs) (err error) {
+	// prepare lists
+	var only, except []string
+	if args.Only != nil && len(*args.Only) > 0 {
+		only = strings.Split(*args.Only, ",")
+	}
+	if args.Except != nil && len(*args.Except) > 0 {
+		except = strings.Split(*args.Except, ",")
+	}
+
+	ch := channel.New(q.Accept(), channel.WithFormats(args.In, args.Out))
+	defer ch.Close()
+
+	for {
+		object, err := ch.Receive()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+			return err
+		}
+
+		if len(only) > 0 {
+			if !slices.Contains(only, object.ObjectType()) {
+				continue
+			}
+		}
+
+		if len(except) > 0 {
+			if slices.Contains(only, object.ObjectType()) {
+				continue
+			}
+		}
+
+		err = ch.Send(object)
+		if err != nil {
+			return err
+		}
+	}
+}
