84f95c9277bbcddc747df5e2bf16277529016327
Rekseto <grzegorz.dyl.private@gmail.com>
2026-01-16 19:31:21 +0000

    mod/fs: ops for indexer

---
+++ mod/fs/src/op_remove_repo.go
@@ -0,0 +1,43 @@
+package fs
+
+import (
+	"github.com/cryptopunkscc/astrald/astral"
+	"github.com/cryptopunkscc/astrald/astral/channel"
+	"github.com/cryptopunkscc/astrald/mod/objects"
+	"github.com/cryptopunkscc/astrald/mod/shell"
+)
+
+type opRemoveRepo struct {
+	Repo string
+	In   string `query:"optional"`
+	Out  string `query:"optional"`
+}
+
+func (mod *Module) OpRemoveRepo(ctx *astral.Context, q shell.Query, args opRemoveRepo) (err error) {
+	ch := q.AcceptChannel(channel.WithFormats(args.In, args.Out))
+	defer ch.Close()
+
+	repo := mod.Objects.GetRepository(args.Repo)
+	if repo == nil {
+		return ch.Send(astral.NewError("repo not found"))
+	}
+
+	err = mod.Objects.RemoveRepository(args.Repo)
+	if err != nil {
+		return ch.Send(astral.NewError(err.Error()))
+	}
+
+	// fixme: hacky
+	// type assert to *WatchRepository
+	watchRepo, ok := repo.(*WatchRepository)
+	if ok {
+		mod.indexer.removeRoot(watchRepo.root)
+	}
+
+	err = mod.Objects.RemoveGroup(objects.RepoLocal, args.Repo)
+	if err != nil {
+		return ch.Send(astral.NewError(err.Error()))
+	}
+
+	return ch.Send(&astral.Ack{})
+}
