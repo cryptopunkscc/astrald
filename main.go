c8de1776386632da399b4ccb21548c96d5a991cb
Rekseto <grzegorz.dyl.private@gmail.com>
2026-01-16 15:02:41 +0000

    astrald: remove testing service discovery in main()

--- main.go
+++ main.go
@@ -1,24 +1,3 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/cryptopunkscc/astrald/lib/astrald"
-)
-
-func main() {
-	ctx := astrald.NewContext()
-
-	ch, err := astrald.Services().Discover(ctx, true)
-	if err != nil {
-		panic(err)
-	}
-
-	for u := range ch {
-		if u == nil {
-			fmt.Println("snapshot done")
-			continue
-		}
-		fmt.Println(u.Available, u.Name, u.ProviderID)
-	}
-}
+func main() {}
