9fb571526b2c98ad278b4a0ec5d63c549f4de2fb
arashi <arashi@cryptopunks.cc>
2026-01-22 03:20:24 +0100

    mod/log: view fix

--- mod/log/string_view.go
+++ mod/log/string_view.go
@@ -1,6 +1,9 @@
 package log
 
 import (
+	"errors"
+	"io"
+
 	"github.com/charmbracelet/lipgloss"
 	"github.com/cryptopunkscc/astrald/astral"
 )
@@ -10,11 +13,21 @@ type StringView struct {
 	String *astral.String32
 }
 
+var _ astral.Object = &StringView{}
+
 // astral:blueprint-ignore
 func (StringView) ObjectType() string { return "" }
 
 func (v StringView) Render() string {
 	return v.Style.Render(v.String.String())
+}
+
+func (StringView) WriteTo(writer io.Writer) (n int64, err error) {
+	return 0, errors.New("WriteTo called on a pseudo-object")
+}
+
+func (StringView) ReadFrom(reader io.Reader) (n int64, err error) {
+	return 0, errors.New("WriteTo called on a pseudo-object")
 }
 
 func String(text string, style *lipgloss.Style) *StringView {
