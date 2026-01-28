63620b9ced542706fca2539771b8aceb8c69bedb
Rekseto <grzegorz.dyl.private@gmail.com>
2026-01-28 12:00:39 +0000

    mod/nat: WIP

--- mod/nat/src/op_start_traversal.go
+++ mod/nat/src/op_start_traversal.go
@@ -5,6 +5,7 @@ import (
 	"github.com/cryptopunkscc/astrald/astral/channel"
 	"github.com/cryptopunkscc/astrald/lib/astrald"
 	"github.com/cryptopunkscc/astrald/lib/ops"
+	"github.com/cryptopunkscc/astrald/mod/nat"
 	natclient "github.com/cryptopunkscc/astrald/mod/nat/client"
 )
 
@@ -56,6 +57,72 @@ func (mod *Module) OpStartTraversal(ctx *astral.Context, q *ops.Query, args opSt
 
 	// Participant flow
 	mod.log.Log("starting traversal as participant with %v", q.Caller())
-	// mod.addTraversedPair(pair, false)
+	exchange := nat.NewPunchExchange(ch)
+	offer, err := exchange.Expect(nat.PunchSignalTypeOffer)
+	if err != nil {
+		return err
+	}
+
+	puncher, err := mod.openPuncher(offer.Session)
+	if err != nil {
+		return err
+	}
+
+	defer puncher.Close()
+
+	err = exchange.Send(&nat.PunchSignal{
+		Signal:  nat.PunchSignalTypeAnswer,
+		Session: puncher.Session(),
+		IP:      localIP,
+		Port:    astral.Uint16(puncher.LocalPort()),
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = exchange.Expect(nat.PunchSignalTypeReady)
+	if err != nil {
+		return err
+	}
+
+	err = exchange.Send(&nat.PunchSignal{
+		Signal:  nat.PunchSignalTypeGo,
+		Session: puncher.Session(),
+	})
+	if err != nil {
+		return err
+	}
+
+	punchResult, err := puncher.HolePunch(ctx, offer.IP, int(offer.Port))
+	if err != nil {
+		return err
+	}
+
+	pair := nat.TraversedPortPair{
+		PeerA: nat.PeerEndpoint{Identity: ctx.Identity()},
+		PeerB: nat.PeerEndpoint{Identity: q.Caller(), Endpoint: nat.UDPEndpoint{IP: punchResult.RemoteIP, Port: punchResult.RemotePort}},
+	}
+
+	err = exchange.Send(&nat.PunchSignal{
+		Signal:    nat.PunchSignalTypeResult,
+		Session:   puncher.Session(),
+		IP:        pair.PeerB.Endpoint.IP,
+		Port:      pair.PeerB.Endpoint.Port,
+		PairNonce: pair.Nonce,
+	})
+	if err != nil {
+		return err
+	}
+
+	result, err := exchange.Expect(nat.PunchSignalTypeResult)
+	if err != nil {
+		return err
+	}
+
+	pair.Nonce = result.PairNonce
+	pair.PeerA.Endpoint = nat.UDPEndpoint{IP: result.IP, Port: result.Port}
+
+	mod.log.Info("NAT traversal § with %v: %v <-> %v", q.Caller(), pair.PeerA.Endpoint, pair.PeerB.Endpoint)
+	mod.addTraversedPair(pair, false)
 	return nil
 }
