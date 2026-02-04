2a6c456c53cda78ffb0bdd0e7b27d4c573d7f1ea
Rekseto <grzegorz.dyl.private@gmail.com>
2026-02-04 23:28:14 +0000

    mod/nodes: cleanup

--- mod/nodes/src/endpoint_resolver.go
+++ mod/nodes/src/endpoint_resolver.go
@@ -1,7 +1,6 @@
 package nodes
 
 import (
-	"slices"
 	"sync"
 
 	"github.com/cryptopunkscc/astrald/astral"
@@ -28,39 +27,6 @@ func (mod *Module) ResolveEndpoints(ctx *astral.Context, nodeID *astral.Identity
 		// wait for all resolvers to finish
 		wg.Wait()
 		close(ch)
-	}()
-
-	return ch, nil
-}
-
-func (mod *Module) ResolveEndpointsFiltered(ctx *astral.Context, target *astral.Identity, o *RetrieveLinkOptions) (_ <-chan exonet.Endpoint, err error) {
-	resolve, err := mod.ResolveEndpoints(ctx, target)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(o.IncludeNetworks) == 0 && len(o.ExcludeNetworks) == 0 {
-		return resolve, nil
-	}
-
-	ch := make(chan exonet.Endpoint)
-	go func() {
-		defer close(ch)
-		for e := range resolve {
-			net := e.Network()
-			if len(o.ExcludeNetworks) > 0 && slices.Contains(o.ExcludeNetworks, net) {
-				continue
-			}
-			if len(o.IncludeNetworks) > 0 && !slices.Contains(o.IncludeNetworks, net) {
-				continue
-			}
-
-			select {
-			case ch <- e:
-			case <-ctx.Done():
-				return
-			}
-		}
 	}()
 
 	return ch, nil
