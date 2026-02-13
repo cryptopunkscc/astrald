6d2503cbef0405eb2eb900a08d557994160d4cce
Rekseto <grzegorz.dyl.private@gmail.com>
2026-02-13 18:02:59 +0000

    mod/nodes: fix panic on opAddEndpoints

--- mod/nodes/src/op_add_endpoint.go
+++ mod/nodes/src/op_add_endpoint.go
@@ -5,12 +5,15 @@ import (
 	"strings"
 
 	"github.com/cryptopunkscc/astrald/astral"
+	"github.com/cryptopunkscc/astrald/astral/channel"
 	"github.com/cryptopunkscc/astrald/lib/ops"
 )
 
 type opAddEndpointArgs struct {
 	ID       *astral.Identity
 	Endpoint string
+	In       string `query:"optional"`
+	Out      string `query:"optional"`
 }
 
 func (mod *Module) OpAddEndpoint(_ *astral.Context, q *ops.Query, args opAddEndpointArgs) (err error) {
@@ -18,14 +21,18 @@ func (mod *Module) OpAddEndpoint(_ *astral.Context, q *ops.Query, args opAddEndp
 	if len(chunks) != 2 {
 		return errors.New("invalid endpoint")
 	}
+
+	ch := channel.New(q.Accept(), channel.WithFormats(args.In, args.Out))
+	defer ch.Close()
+
 	parse, err := mod.Exonet.Parse(chunks[0], chunks[1])
 	if err != nil {
-		return
+		return ch.Send(astral.Err(err))
 	}
 	err = mod.AddEndpoint(args.ID, parse)
 	if err != nil {
-		return
+		return ch.Send(astral.Err(err))
 	}
-	_ = q.Accept().Close()
-	return
+
+	return ch.Send(&astral.Ack{})
 }
