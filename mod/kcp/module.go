b3b933a5dba3fa558b41e9deb4f09a60f21f2819
Rekseto <grzegorz.dyl.private@gmail.com>
2026-02-18 13:58:36 +0000

    mod/kcp: client for KCP operations

--- mod/kcp/module.go
+++ mod/kcp/module.go
@@ -6,6 +6,14 @@ import (
 
 const ModuleName = "kcp"
 
+const (
+	MethodNewEphemeralListener          = "kcp.new_ephemeral_listener"
+	MethodCloseEphemeralListener        = "kcp.close_ephemeral_listener"
+	MethodAddRemoteEndpointLocalPort    = "kcp.add_remote_endpoint_local_port"
+	MethodRemoveRemoteEndpointLocalPort = "kcp.remove_remote_endpoint_local_port"
+	MethodListEndpointsLocalMappings    = "kcp.list_endpoints_local_mappings"
+)
+
 type Module interface {
 	exonet.Dialer
 	exonet.Unpacker
