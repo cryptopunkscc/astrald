dd5ca75523aae25c15c2eaa5888b2a38c53dfc5b
yang <cryptopunkyang@pm.me>
2026-03-08 19:54:42 +0100

    mod/objects/client: store

---
+++ mod/objects/client/store.go
@@ -0,0 +1,20 @@
+package objects
+
+import (
+	"github.com/cryptopunkscc/astrald/astral"
+	"github.com/cryptopunkscc/astrald/astral/channel"
+	"github.com/cryptopunkscc/astrald/lib/query"
+)
+
+func (client *Client) Store(ctx *astral.Context, repo string, object astral.Object) (id *astral.ObjectID, err error) {
+	ch, err := client.queryCh(ctx, "objects.store", query.Args{"repo": repo})
+	if err != nil {
+		return
+	}
+	defer ch.Close()
+	if err = ch.Send(object); err != nil {
+		return
+	}
+	err = ch.Switch(channel.Expect(&id), channel.PassErrors)
+	return
+}
