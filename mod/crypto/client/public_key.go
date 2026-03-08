d86e303d87f42d28f4e6d94ee2150f968419216a
yang <cryptopunkyang@pm.me>
2026-03-08 19:52:22 +0100

    mod/crypto/client: public_key

---
+++ mod/crypto/client/public_key.go
@@ -0,0 +1,20 @@
+package crypto
+
+import (
+	"github.com/cryptopunkscc/astrald/astral"
+	"github.com/cryptopunkscc/astrald/astral/channel"
+	"github.com/cryptopunkscc/astrald/mod/crypto"
+)
+
+func (client *Client) PublicKey(ctx *astral.Context, privateKey *crypto.PrivateKey) (publicKey *crypto.PublicKey, err error) {
+	ch, err := client.queryCh(ctx, "crypto.public_key", nil)
+	if err != nil {
+		return
+	}
+	defer ch.Close()
+	if err = ch.Send(privateKey); err != nil {
+		return
+	}
+	err = ch.Switch(channel.Expect(&publicKey), channel.PassErrors)
+	return
+}
