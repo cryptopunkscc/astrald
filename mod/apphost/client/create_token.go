87ee1b8c3b539fbdba612831c66675bf3bbb8a76
yang <cryptopunkyang@pm.me>
2026-03-12 11:38:06 +0100

    mod/apphost/client: create_token

---
+++ mod/apphost/client/create_token.go
@@ -0,0 +1,18 @@
+package apphost
+
+import (
+	"github.com/cryptopunkscc/astrald/astral"
+	"github.com/cryptopunkscc/astrald/astral/channel"
+	"github.com/cryptopunkscc/astrald/lib/query"
+	"github.com/cryptopunkscc/astrald/mod/apphost"
+)
+
+func (client *Client) CreateToken(ctx *astral.Context, identity *astral.Identity) (token *apphost.AccessToken, err error) {
+	ch, err := client.queryCh(ctx, "apphost.create_token", query.Args{"id": identity.String()})
+	if err != nil {
+		return
+	}
+	defer ch.Close()
+	err = ch.Switch(channel.Expect(&token), channel.PassErrors)
+	return
+}
