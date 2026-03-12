f70d5bb678d48f8abe93c3f0e180f2d3496eabc8
Rekseto <grzegorz.dyl.private@gmail.com>
2026-03-12 15:24:14 +0000

    mod/gateway: improve socket pool

--- mod/gateway/errors.go
+++ mod/gateway/errors.go
@@ -5,3 +5,4 @@ import "errors"
 var ErrUnauthorized = errors.New("unauthorized")
 var ErrTargetNotReachable = errors.New("target not reachable")
 var ErrInvalidGateway = errors.New("invalid gateway")
+var ErrSocketUnreachable = errors.New("socket unreachable")
