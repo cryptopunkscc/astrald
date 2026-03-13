c2a85fe6e3e4e185fd7ab8707be2f413db60a3c6
Rekseto <grzegorz.dyl.private@gmail.com>
2026-03-13 16:47:47 +0000

    remove outdated files

--- mod/tcp/src/net.go
+++
@@ -1,5 +0,0 @@
-package tcp
-
-import "net"
-
-var InterfaceAddrs = net.InterfaceAddrs
--- mod/utp/src/net.go
+++
@@ -1,7 +0,0 @@
-package utp
-
-import "net"
-
-// NOTE: same as in tcp module - consider moving to a common package
-
-var InterfaceAddrs = net.InterfaceAddrs
