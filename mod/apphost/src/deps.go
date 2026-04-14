628e7b50ca29e7f6a9f0005f444e026f2cabebe6
Rekseto <grzegorz.dyl.private@gmail.com>
2026-04-14 17:18:34 +0100

    WIP

--- mod/apphost/src/deps.go
+++ mod/apphost/src/deps.go
@@ -3,14 +3,12 @@ package apphost
 import (
 	"github.com/cryptopunkscc/astrald/astral"
 	"github.com/cryptopunkscc/astrald/core"
-	"github.com/cryptopunkscc/astrald/mod/auth"
-	"github.com/cryptopunkscc/astrald/mod/nodes"
 )
 
 func (mod *Module) LoadDependencies(*astral.Context) (err error) {
 	if err = core.Inject(mod.node, &mod.Deps); err != nil {
 		return
 	}
-	mod.Auth.Add(nodes.ActionRelayFor, auth.Func[*nodes.RelayForAction](mod.AuthorizeNodesRelayFor))
+
 	return
 }
