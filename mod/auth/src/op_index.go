2fea7d36a24c4cb3504d56d48f7329379da8804a
Rekseto <grzegorz.dyl.private@gmail.com>
2026-04-15 11:06:37 +0100

    better op index in auth

--- mod/auth/src/op_index.go
+++ mod/auth/src/op_index.go
@@ -8,37 +8,23 @@ import (
 )
 
 type opIndexArgs struct {
-	In  string `query:"optional"`
-	Out string `query:"optional"`
+	ID  *astral.ObjectID `query:"required"`
+	In  string           `query:"optional"`
+	Out string           `query:"optional"`
 }
 
 func (mod *Module) OpIndex(ctx *astral.Context, q *ops.Query, args opIndexArgs) error {
 	ch := q.AcceptChannel(channel.WithFormats(args.In, args.Out))
 	defer ch.Close()
 
-	var signed *auth.SignedContract
-
-	err := ch.Switch(
-		func(objectID *astral.ObjectID) error {
-			object, loadErr := mod.Objects.Load(ctx, mod.Objects.ReadDefault(), objectID)
-			if loadErr != nil {
-				return loadErr
-			}
-			var ok bool
-			signed, ok = object.(*auth.SignedContract)
-			if !ok {
-				return auth.ErrInvalidContract
-			}
-			return channel.ErrBreak
-		},
-		func(sc *auth.SignedContract) error {
-			signed = sc
-			return channel.ErrBreak
-		},
-		channel.PassErrors,
-	)
+	object, err := mod.Objects.Load(ctx, mod.Objects.ReadDefault(), args.ID)
 	if err != nil {
 		return ch.Send(astral.Err(err))
+	}
+
+	signed, ok := object.(*auth.SignedContract)
+	if !ok {
+		return ch.Send(auth.ErrInvalidContract)
 	}
 
 	err = mod.IndexContract(ctx, signed)
@@ -46,5 +32,5 @@ func (mod *Module) OpIndex(ctx *astral.Context, q *ops.Query, args opIndexArgs) 
 		return ch.Send(astral.Err(err))
 	}
 
-	return ch.Send(&astral.EOS{})
+	return ch.Send(&astral.Ack{})
 }
