1d3b6315c488016bcf0ffd58e8e302e09658b09f
Rekseto <grzegorz.dyl.private@gmail.com>
2026-05-08 11:00:15 +0100

    mod/nodes: remove streams file

--- mod/nodes/frames/frame.go
+++ mod/nodes/frames/frame.go
@@ -11,6 +11,20 @@ type Frame interface {
 	fmt.Stringer
 }
 
+var FrameTypes = []string{
+	"nodes.frames.ping",
+	"nodes.frames.query",
+	"nodes.frames.relay_query",
+	"nodes.frames.read",
+	"nodes.frames.response",
+	"nodes.frames.data",
+	"nodes.frames.migrate",
+	"nodes.frames.reset",
+}
+
+var FrameTypeEncoder = astral.IndexedTypeEncoder(FrameTypes)
+var FrameTypeDecoder = astral.IndexedTypeDecoder(FrameTypes)
+
 func init() {
 	_ = astral.Add(
 		&Ping{},
